62d737d28cf62880e6c74ee515ce0d479b28e6e7
Fiz <fizreyarmesto@gmail.com>
2022-11-15 17:58:29 +0100

    Co-authored-by: nicola-22 <nicola-22@users.noreply.github.com>

---
+++ Nuevo/clientcp.c
@@ -0,0 +1,65 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+#include <time.h>
+
+#define PUERTO 17278
+#define TAM_BUFFER 10
+
+int main(argc, argv)
+int argc;
+char *argv[];
+
+{
+    int s;				/* connected socket descriptor */
+   	struct addrinfo hints, *res;
+    long timevar;			/* contains time returned by time() */
+    struct sockaddr_in myaddr_in;	/* for local socket address */
+    struct sockaddr_in servaddr_in;	/* for server socket address */
+	int addrlen, i, j, errcode;
+    /* This example uses TAM_BUFFER byte messages. */
+	char buf[TAM_BUFFER];
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage:  %s <remote host>\n", argv[0]);
+		exit(1);
+	}
+
+	/* Create the socket. */
+	s = socket (AF_INET, SOCK_STREAM, 0);
+	if (s == -1) {
+		perror(argv[0]);
+		fprintf(stderr, "%s: unable to create socket\n", argv[0]);
+		exit(1);
+	}
+    	/* clear out address structures */
+	memset ((char *)&myaddr_in, 0, sizeof(struct sockaddr_in));
+	memset ((char *)&servaddr_in, 0, sizeof(struct sockaddr_in));
+
+	/* Set up the peer address to which we will connect. */
+	servaddr_in.sin_family = AF_INET;
+	
+	/* Get the host information for the hostname that the
+	 * user passed in. */
+      memset (&hints, 0, sizeof (hints));
+      hints.ai_family = AF_INET;
+ 	 /* esta funci�n es la recomendada para la compatibilidad con IPv6 gethostbyname queda obsoleta*/
+    errcode = getaddrinfo (argv[1], NULL, &hints, &res); 
+    if (errcode != 0){
+			/* Name was not found.  Return a
+			 * special value signifying the error. */
+		fprintf(stderr, "%s: No es posible resolver la IP de %s\n",
+				argv[0], argv[1]);
+		exit(1);
+        }
+    else {
+		/* Copy address of host */
+		servaddr_in.sin_addr = ((struct sockaddr_in *) res->ai_addr)->sin_addr;
+	    }
+    freeaddrinfo(res);
