53cb1097cf9e09794ab43d13cb457084f54f7806
Fiz <fizreyarmesto@gmail.com>
2022-12-10 04:44:56 +0100

    espacios + fgets

--- New/cliente.c
+++ New/cliente.c
@@ -219,7 +219,7 @@ char *argv[];
 		contents = (char*) malloc (516);
 		respuesta = (char*) malloc (516);
 
-		while(fgets(&contents,516,input_file) != NULL)
+		while(fgets(contents,516,input_file) != NULL)
 		{
 			if ((strstr(contents, ".\r\n") != NULL) && (strlen(contents) == 3))
 				cuerpoCorreo = 0;
@@ -325,7 +325,7 @@ char *argv[];
 		contents = (char*) malloc (516);
 		respuesta = (char*) malloc (516);
 		int n_intentos;
-		while(fgets(&contents,516,input_file) != NULL)
+		while(fgets(contents,516,input_file) != NULL)
 		{
 			n_intentos = 0;
 			while(n_intentos < MAX_INTENTOS){					
